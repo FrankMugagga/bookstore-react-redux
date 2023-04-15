70cba35b143c80bf8b0de2e46c504f43c505afee
FrankMugagga <frank.mugagga@gmail.com>
2023-04-15 14:22:24 +0300

    made the display dynamic for inputting the author name, category, and title

--- src/Components/BookList.js
+++ src/Components/BookList.js
@@ -1,160 +1,71 @@
 import React from 'react';
+import { PropTypes } from 'prop-types';
 
-function BookList() {
+function BookList({ booksL }) {
   return (
     <ul>
-      <li>
-        <div className="bookInfo">
 
-          <div className="bookDetails">
-            <div className="category">
-              Action
-            </div>
-            <div className="title">
-              The hunger Games
-            </div>
-            <div className="author">
-              Suzanne Collins
-            </div>
+      {
+        booksL.map((books) => (
+          <li key={books.id}>
+            <div className="bookInfo">
 
-            <div className="controlBtn">
-              <button className="ctrlBtn" type="submit">Comments</button>
-              <button className="ctrlBtn btn2" type="submit">Remove</button>
-              <button className="ctrlBtn" type="submit">Edit</button>
-            </div>
+              <div className="bookDetails">
+                <div className="category">
+                  {books.category}
+                </div>
+                <div className="title">
+                  {books.title}
+                </div>
+                <div className="author">
+                  {books.title}
+                </div>
 
-          </div>
+                <div className="controlBtn">
+                  <button className="ctrlBtn" type="submit">Comments</button>
+                  <button className="ctrlBtn btn2" type="submit">Remove</button>
+                  <button className="ctrlBtn" type="submit">Edit</button>
+                </div>
 
-          <div className="loading">
-            <div className="loadingCircle" />
-            <div className="loadDetails">
-              <div className="loadNum">
-                64%
-              </div>
-              <div className="loadstatus">
-                Completed
               </div>
 
-            </div>
-          </div>
+              <div className="loading">
+                <div className="loadingCircle" />
+                <div className="loadDetails">
+                  <div className="loadNum">
+                    64%
+                  </div>
+                  <div className="loadstatus">
+                    Completed
+                  </div>
 
-        </div>
-
-        <div className="chapters">
-          <h3 className="h3h">
-            CURRENT CHAPTER
-          </h3>
-          <h5 className="h5h">
-            Chapter 17
-          </h5>
-          <buuton className="updatebtn" type="submit">
-            UPDATE PROGRESS
-          </buuton>
-        </div>
-
-      </li>
-
-      <li>
-        <div className="bookInfo">
-
-          <div className="bookDetails">
-            <div className="category">
-              Science Fiction
-            </div>
-            <div className="title">
-              Dune
-            </div>
-            <div className="author">
-              Frank Herbert
-            </div>
-
-            <div className="controlBtn">
-              <button className="ctrlBtn" type="submit">Comments</button>
-              <button className="ctrlBtn btn2" type="submit">Remove</button>
-              <button className="ctrlBtn" type="submit">Edit</button>
-            </div>
-
-          </div>
-
-          <div className="loading">
-            <div className="loadingCircle" />
-            <div className="loadDetails">
-              <div className="loadNum">
-                8%
-              </div>
-              <div className="loadstatus">
-                Completed
+                </div>
               </div>
 
             </div>
-          </div>
 
-        </div>
-
-        <div className="chapters">
-          <h3>
-            CURRENT CHAPTER
-          </h3>
-          <h5>
-            Chapter 3: `A lesson Learned `
-          </h5>
-          <buuton className="updatebtn" type="submit">
-            UPDATE PROGRESS
-          </buuton>
-        </div>
-      </li>
-      <li>
-        <div className="bookInfo">
-
-          <div className="bookDetails">
-            <div className="category">
-              Economy
-            </div>
-            <div className="title">
-              Capital in the Twenty-First Century
-            </div>
-            <div className="author">
-              Suzanne Collins
+            <div className="chapters">
+              <h3 className="h3h">
+                CURRENT CHAPTER
+              </h3>
+              <h5 className="h5h">
+                Chapter 17
+              </h5>
+              <buuton className="updatebtn" type="submit">
+                UPDATE PROGRESS
+              </buuton>
             </div>
 
-            <div className="controlBtn">
-              <button className="ctrlBtn" type="submit">Comments</button>
-              <button className="ctrlBtn btn2" type="submit">Remove</button>
-              <button className="ctrlBtn" type="submit">Edit</button>
-            </div>
+          </li>
 
-          </div>
-
-          <div className="loading">
-            <div className="loadingCircle" />
-            <div className="loadDetails">
-              <div className="loadNum">
-                0%
-              </div>
-              <div className="loadstatus">
-                Completed
-              </div>
-
-            </div>
-          </div>
-
-        </div>
-
-        <div className="chapters">
-          <h3>
-            CURRENT CHAPTER
-          </h3>
-          <h5>
-            Introduction
-          </h5>
-          <buuton className="updatebtn" type="submit">
-            UPDATE PROGRESS
-          </buuton>
-        </div>
-
-      </li>
+        ))
+      }
     </ul>
   );
 }
+
+BookList.propTypes = {
+  booksL: PropTypes.objectOf(PropTypes.string).isRequired,
+};
 
 export default BookList;
--- src/Components/Books.js
+++ src/Components/Books.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import { React, useState } from 'react';
 import Form from './Form';
 import BookList from './BookList';
 import './Books.css';
 
 function Books() {
+  const [books, setBooks] = useState([
+    {
+      id: 1,
+      category: 'Action',
+      title: 'The hunger Games',
+      author: 'Suzanne Collins',
+    },
+    {
+      id: 2,
+      category: 'Science Fiction',
+      title: 'Dune',
+      author: 'Frank Herbert',
+    },
+    {
+      id: 3,
+      category: 'Economy',
+      title: 'Capital in the Twenty-First Century',
+      author: 'Suzanne Collins',
+    },
+  ]);
   return (
     <div className="booksMain">
-      <BookList />
+      <BookList setBooks={setBooks} booksL={books} />
       <Form />
     </div>
   );
