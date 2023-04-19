a7375ed231897aade6a4c80a5d41f4d63aa3940a
FrankMugagga <frank.mugagga@gmail.com>
2023-04-19 23:12:54 +0300

    set up the book reducers and actions

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,16 +1,46 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  value: [],
+  book: [
+    {
+      item_id: 'item1',
+      title: 'The Great Gatsby',
+      author: 'John Smith',
+      category: 'Fiction',
+    },
+    {
+      item_id: 'item2',
+      title: 'Anna Karenina',
+      author: 'Leo Tolstoy',
+      category: 'Fiction',
+    },
+    {
+      item_id: 'item3',
+      title: 'The Selfish Gene',
+      author: 'Richard Dawkins',
+      category: 'Nonfiction',
+    },
+  ],
 };
 
-export const booksSlice = createSlice({
+const booksSlice = createSlice({
   name: 'books',
   initialState,
 
   reducers: {
-    addbook: { },
-    removebook: { },
+    addbook: (state, action) => {
+      const newBook = {
+        item_id: `item${state.book.length + 1}`,
+        title: action.payload.title,
+        author: action.payload.author,
+        category: action.payload.category,
+      };
+      state.book.push(newBook);
+    },
+    removebook: (state, action) => {
+      const bookId = action.payload;
+      state.book = state.book.filter((book) => book.item_id !== bookId);
+    },
 
   },
 });
