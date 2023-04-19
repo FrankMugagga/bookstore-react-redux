a4bce90334561ac9d02966014884ea78f273dade
FrankMugagga <frank.mugagga@gmail.com>
2023-04-19 23:10:17 +0300

    Update the the categories files

--- src/Components/Categories.js
+++ src/Components/Categories.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 function Categories() {
-  return (<div>Categories</div>);
+  const check = useSelector((store) => store.Categories.check);
+
+  return (
+    <div>
+      <h3>{check}</h3>
+      <button type="submit" onClick={alert('click')}>Check</button>
+    </div>
+  );
 }
 
 export default Categories;
