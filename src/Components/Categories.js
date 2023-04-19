1e86ff191cd9e42c74071bbfaf23c3eff88cf342
FrankMugagga <frank.mugagga@gmail.com>
2023-04-19 23:22:25 +0300

    update categories

--- src/Components/Categories.js
+++ src/Components/Categories.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 
 function Categories() {
-  const check = useSelector((store) => store.Categories.check);
-
   return (
     <div>
-      <h3>'Under construction'
-        
+      <h3>
+        Under construction
       </h3>
-      <button type="submit" onClick={alert('click')}>Check</button>
+      <button type="submit">Check</button>
     </div>
   );
 }
