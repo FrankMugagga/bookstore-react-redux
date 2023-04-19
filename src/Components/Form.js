5b47b9a20c8c0970838124d16d51029f1c62b11d
FrankMugagga <frank.mugagga@gmail.com>
2023-04-19 23:12:09 +0300

    update the form to receive data and with an input for the author

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addbook } from '../redux/books/booksSlice';
 
 function Form() {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [category, setCategory] = useState('');
+
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(addbook({ title, author, category }));
+    setTitle('');
+    setAuthor('');
+    setCategory('');
+  };
+
   return (
 
-    <form>
+    <form onSubmit={handleSubmit}>
       <label htmlFor="book">
-        <input id="book" name="book" type="text" placeholder="Book title" />
+        <input id="book" name="book" type="text" placeholder="Book title" onChange={(e) => setTitle(e.target.value)} />
+      </label>
+
+      <label htmlFor="author">
+        <input id="author" name="author" type="text" placeholder="Author" onChange={(e) => setAuthor(e.target.value)} />
       </label>
 
       <label htmlFor="category">
-        <select id="category" name="category" value="Category" placeholder="Book title">
-          <option value="action">Category</option>
-          <option value="action">Action</option>
-          <option value="science fiction">Science Fiction</option>
-          <option value="economy">Economy</option>
+        <select id="category" name="category" value={category} onChange={(e) => setCategory(e.target.value)}>
+          <option value="">Category</option>
+          <option value="Action">Action</option>
+          <option value="Science fiction">Science Fiction</option>
+          <option value="Economy">Economy</option>
         </select>
       </label>
 
